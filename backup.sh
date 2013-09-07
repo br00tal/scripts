af7015817690a3d943ac0d7af8f7a6a95c666989
br00tal <br00tal@fakeemail.com>
2013-09-07 10:50:47 -0500

    Added comments, removed /auto, etc.

--- backup.sh
+++ backup.sh
@@ -12,8 +12,9 @@ cd $BACKUPDIR
 # Keep four backups total, and remove anything older
 ls -t ${HOSTNAME}*.tar.xz | sed -e '1,3d' | xargs -d '\n' rm
 
+# Start the rsync operation
 START=$(date +%s)
-rsync -aAXv /* $BACKUPDIR/$HOSTNAME-$DATE --exclude auto/* --exclude dev/* --exclude proc/* --exclude sys/* --exclude tmp/* --exclude run/* --exclude mnt/* --exclude media/* --exclude lost+found --exclude var/lib/pacman/sync/*
+rsync -aAXv /* $BACKUPDIR/$HOSTNAME-$DATE --exclude dev/* --exclude proc/* --exclude sys/* --exclude tmp/* --exclude run/* --exclude mnt/* --exclude media/* --exclude lost+found --exclude var/lib/pacman/sync/*
 FINISH=$(date +%s)
 echo "total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
 

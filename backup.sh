c2afd5d442a4c65eba235c30898e720eb8d0fbba
br00tal <br00tal@fakeemail.com>
2013-09-10 13:01:14 -0500

    Added logline function, log file deletion, etc.

--- backup.sh
+++ backup.sh
@@ -13,6 +13,11 @@ BACKUPLOG=$BACKUPDIR/$HOSTNAME-$DATE.log
 RSYNCOPTS="-aAXv --progress"
 ARMOUNT=`which archivemount`
 
+logline () {
+  message=$1
+  echo "[`date +%m/%d/%y\ %H:%M:%S`] $message"
+}
+
 # Print initial script start time
 echo "Backup of $HOSTNAME starting at $TIME" >> $BACKUPLOG
 
@@ -21,19 +26,22 @@ mkdir $BACKUPDIR/$HOSTNAME-$DATE
 cd $BACKUPDIR
 
 # Keep four backups total, and remove anything older
-echo "Removing old backups for $HOSTNAME" >> $BACKUPLOG
+logline "Removing old backups for $HOSTNAME" >> $BACKUPLOG
 ls -t ${HOSTNAME}*.tar.xz | sed -e '1,3d' | xargs -d '\n' rm
+logline "Removing old backup logs for $HOSTNAME" >> $BACKUPLOG
+ls -t ${HOSTNAME}*.log | sed -e '1,3d' | xargs -d '\n' rm
+
 
 # If archivemount is found, create an empty tarball for use
 if [ -f "$ARMOUNT" ]; then
-  echo "$ARMOUNT found, using direct .tar.xz writing."
-  echo "Creating empty $HOSTNAME-$DATE.tar.xz file for writing..."
+  logline "$ARMOUNT found, using direct .tar.xz writing." >> $BACKUPLOG
+  logline "Creating empty $HOSTNAME-$DATE.tar.xz file for writing..." >> $BACKUPLOG
   tar -c --xz -f $HOSTNAME-$DATE.tar.xz -T /dev/null
   exit 0
 fi
 
 # Start the rsync operation
-echo "Starting the rsync operation for $HOSTNAME" >> $BACKUPLOG
+logline "Starting the rsync operation for $HOSTNAME" >> $BACKUPLOG
 START=$(date +%s)
 rsync $RSYNCOPTS /* $BACKUPDIR/$HOSTNAME-$DATE --exclude dev/* \
   --exclude proc/* --exclude sys/* --exclude tmp/* --exclude run/* \
@@ -41,11 +49,11 @@ rsync $RSYNCOPTS /* $BACKUPDIR/$HOSTNAME-$DATE --exclude dev/* \
   --exclude var/lib/pacman/sync/*
 
 # Convert the directory to an xz archive for space purposes
-echo "Starting the compression operation for $HOSTNAME" >> $BACKUPLOG
+logline "Starting the compression operation for $HOSTNAME" >> $BACKUPLOG
 cd $BACKUPDIR
 tar -c --xz -f $HOSTNAME-$DATE.tar.xz $HOSTNAME-$DATE
 rm -rf $HOSTNAME-$DATE
 
 # Return the total time from rsync to tar completion
 FINISH=$(date +%s)
-echo "Backup of $HOSTNAME complete.  Total run time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
+logline "Backup of $HOSTNAME complete.  Total run time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"

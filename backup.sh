e4c3aa60838105ff2e6d59074154b1e43f8658a5
br00tal <br00tal@fakeemail.com>
2013-09-12 10:12:56 -0500

    Allowing for xz, bz2, and gz compression methods.  Better logging, etc.

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# backup.sh - A script to backup systems via rsync and tar/xz.
+# backup.sh - A script to backup systems via rsync and tar.
 #
 # Written by br00tal
 #
@@ -12,29 +12,53 @@ BACKUPDIR=/mnt/backup
 BACKUPLOG=$BACKUPDIR/$HOSTNAME-$DATE.log
 RSYNCOPTS="-aAXv --progress"
 
+# Compression method.  Valid values are xz, bzip2, and gzip.
+COMP="xz"
+
+if [ "$COMP" == "xz" ]; then
+  EXT="xz"
+elif [ "$COMP" == "bzip2" ]; then
+  EXT="bz2"
+elif [ "$COMP" == "gzip" ]; then
+  EXT="gz"
+else
+  echo "Invalid compression method.  Exiting..."
+  exit 1
+fi
+
+logline () {
+   message=$1
+   echo "[`date +%m/%d/%y\ %H:%M:%S`] $message"
+}
+
 # Print initial script start time
-echo "Backup of $HOSTNAME starting at $TIME" >> $BACKUPLOG
+logline "Backup of $HOSTNAME starting..." >> $BACKUPLOG
 
 # Create required directories and such
 mkdir $BACKUPDIR/$HOSTNAME-$DATE
 cd $BACKUPDIR
 
 # Keep four backups total, and remove anything older
-echo "Removing old backups for $HOSTNAME" >> $BACKUPLOG
-ls -t ${HOSTNAME}*.tar.xz | sed -e '1,3d' | xargs -d '\n' rm
+logline "Removing old backups for $HOSTNAME" >> $BACKUPLOG
+ls -t ${HOSTNAME}*.tar.$EXT | sed -e '1,3d' | xargs -d '\n' rm > /dev/null 2>&1
+logline "Removing old logs for $HOSTNAME" >> $BACKUPLOG
+ls -t ${HOSTNAME}*.log | sed -e '1,3d' | xargs -d '\n' rm > /dev/null 2>&1
 
 # Start the rsync operation
-echo "Starting the rsync operation for $HOSTNAME" >> $BACKUPLOG
+logline "Starting the rsync operation for $HOSTNAME" >> $BACKUPLOG
 START=$(date +%s)
-rsync $RSYNCOPTS /* $BACKUPDIR/$HOSTNAME-$DATE --exclude dev/* --exclude proc/* --exclude sys/* --exclude tmp/* --exclude run/* --exclude mnt/* --exclude media/* --exclude lost+found --exclude var/lib/pacman/sync/*
+rsync $RSYNCOPTS /* $BACKUPDIR/$HOSTNAME-$DATE --exclude dev/* \
+  --exclude proc/* --exclude sys/* --exclude tmp/* --exclude run/* \
+  --exclude mnt/* --exclude media/* --exclude lost+found \
+  --exclude var/lib/pacman/sync/*
 
 # Convert the directory to an xz archive for space purposes
-echo "Starting the compression operation for $HOSTNAME" >> $BACKUPLOG
+logline "Starting the compression operation for $HOSTNAME" >> $BACKUPLOG
 cd $BACKUPDIR
-tar -c --xz -f $HOSTNAME-$DATE.tar.xz $HOSTNAME-$DATE
+tar -c --$COMP -f $HOSTNAME-$DATE.tar.$EXT $HOSTNAME-$DATE
 rm -rf $HOSTNAME-$DATE
 
 # Return the total time from rsync to tar completion
 FINISH=$(date +%s)
-echo "Backup of $HOSTNAME complete.  Total run time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
+logline "Backup of $HOSTNAME complete.  Total run time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds" >> $BACKUPLOG
 

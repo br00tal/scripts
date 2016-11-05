87539b01e2f63893de7638d4b4fc4c9344d3b8b0
br00tal <br00tal@fakeemail.com>
2016-11-04 20:09:09 -0500

    Updated to optionally use Pushover.

--- godaddy/godaddy.rb
+++ godaddy/godaddy.rb
@@ -14,13 +14,32 @@ api_key = full_config['api_key']
 api_secret = full_config['api_secret']
 domain = full_config['domain']
 ip_provider = full_config['ip_provider']
+po_u_token = full_config['pushover_user_token']
+po_a_token = full_config['pushover_app_token']
 subdomains = full_config['subdomains']
 ttl = full_config['ttl']
+
+if po_u_token.nil?
+  use_pushover = false
+else
+  require 'pushover'
+  use_pushover = true
+end
 
 # This function writes timestamped output
 def logline(message)
   t = Time.now.strftime('%Y-%m-%d %H:%M:%S')
   puts '[' + t + '] ' + message
+end
+
+def pushover(u_token, a_token, record, dns_ip, current_ip)
+  Pushover.configure do |c|
+    c.user = u_token
+    c.token = a_token
+  end
+  message = "IP for #{record} is set to #{dns_ip} in DNS,"\
+            " updating it to the current IP of #{current_ip}"
+  Pushover.notification(message: message, title: 'IP changed!')
 end
 
 logline("START #{__FILE__}")
@@ -37,7 +56,6 @@ a_records = api.get("/v1/domains/#{domain}/records/A")
 # Check the A records against our managed list, and update if necessary
 a_records.each do |a|
   if subdomains.include? a['name']
-    logline " #{a['name']} is set to be managed, comparing IPs..."
     if a['data'] != public_ip
       logline " #{a['name']} is #{a['data']}, not #{public_ip}, updating it..."
       api.put(
@@ -51,10 +69,12 @@ a_records.each do |a|
           }
         ]
       )
+      if use_pushover
+        pushover(po_u_token, po_a_token, a['name'], a['data'], public_ip)
+      end
     elsif a['data'] == public_ip
       logline " #{a['name']}'s IPs match, doing nothing..."
     else
-      logline ' Something strange happened, exiting...'
       exit 1
     end
   else
